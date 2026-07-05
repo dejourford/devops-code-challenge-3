7289072a4b4d2a1b8ad3148701878d1bd0f2d70b
dejourford <dejourford@my.unt.edu>
2026-07-05 12:58:36 -0500

    remove duplicate output

--- terraform/ouputs.tf
+++ terraform/ouputs.tf
@@ -14,10 +14,6 @@ output "backend_repo_url" {
   value = module.ecr.repository_urls["backend"]
 }
 
-output "jenkins_public_ip" {
-  value = module.jenkins.jenkins_public_ip
-}
-
 output "lb_controller_role_arn" {
   value = module.iam.lb_controller_role_arn
 }
