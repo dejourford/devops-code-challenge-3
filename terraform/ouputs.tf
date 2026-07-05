1e3a4097eabc2295c8514538729103db562e9f83
dejourford <dejourford@my.unt.edu>
2026-07-05 12:47:38 -0500

    add lb_controller_role_arn output to root

--- terraform/ouputs.tf
+++ terraform/ouputs.tf
@@ -17,3 +17,7 @@ output "backend_repo_url" {
 output "jenkins_public_ip" {
   value = module.jenkins.jenkins_public_ip
 }
+
+output "lb_controller_role_arn" {
+  value = module.iam.lb_controller_role_arn
+}
